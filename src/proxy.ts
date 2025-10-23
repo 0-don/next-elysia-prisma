f0a2936ee7b399b0b6f05059ed6f05286e9586fc
0-don <don.cryptus@gmail.com>
2025-10-23 19:32:04 +0200

    chore: update dependencies and improve TypeScript configuration; remove unused middleware and restore proxy functionality

--- src/proxy.ts
+++ src/proxy.ts
@@ -4,7 +4,7 @@ import { SERVER_URL_KEY } from "./lib/utils/constants";
 /**
  * Nextjs bypass to set the server URL in the request headers so it can be read while in server components.
  */
-export default async function middleware(request: NextRequest) {
+export default async function proxy(request: NextRequest) {
   const headers = new Headers(request.headers);
   headers.set(SERVER_URL_KEY, request.url);
   return NextResponse.next({ headers });
