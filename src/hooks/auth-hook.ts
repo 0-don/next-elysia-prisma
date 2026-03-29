297175894d5c51ac5346e6b14b3d368c9dc09ff1
0-don <don.cryptus@gmail.com>
2026-03-29 01:38:04 +0100

    fix: update auth hooks to use EdenArgs for argument typing in mutations

--- src/hooks/auth-hook.ts
+++ src/hooks/auth-hook.ts
@@ -1,19 +1,20 @@
 import { rpc } from "@/lib/rpc";
+import { EdenArgs } from "@/lib/types/eden";
 import { handleEden } from "@/lib/utils/base";
 import { useMutation } from "@tanstack/react-query";
 
 export function useRegisterMutation() {
   return useMutation({
     mutationFn: async (
-      args: Parameters<typeof rpc.api.auth.register.post>[0]
-    ) => handleEden(await rpc.api.auth.register.post(args)),
+      args: EdenArgs<typeof rpc.api.auth.register, "post">,
+    ) => handleEden(await rpc.api.auth.register.post(args.body)),
   });
 }
 
 export function useLoginMutation() {
   return useMutation({
-    mutationFn: async (args: Parameters<typeof rpc.api.auth.login.post>[0]) =>
-      handleEden(await rpc.api.auth.login.post(args)),
+    mutationFn: async (args: EdenArgs<typeof rpc.api.auth.login, "post">) =>
+      handleEden(await rpc.api.auth.login.post(args.body)),
   });
 }
 
